9a2c9ff5a0a85ac69211302fe1e3034cd22dadf7
Panthaweekan <tatanoobza1234@hotmail.com>
2021-11-26 16:30:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int interger ;
+    int Even = 0 ;
+    int Odd = 0 ;
+    while ( interger != 0 ){
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> interger ;
+        if( interger % 2 == 0){
+            Even++;
+            if( interger == 0 ){
+                Even--;
+            }
+
+        }else{
+            Odd++;
+        }
+
+    }
+    cout << "#Even numbers = "<< Even << "\n";
+    cout << "#Odd numbers = " << Odd;
     return 0;
 }
